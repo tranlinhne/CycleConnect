0bf972529994fb85dcb3e398c83097c8d85ca972
DT0712 <ducduy102938@gmail.com>
2026-04-10 22:13:23 +0700

    Update

---
+++ User/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "greenride"; // đổi đúng tên database của bạn
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Lỗi kết nối DB: " . mysqli_connect_error());
+}
+?>
